e8e6d15e3c8259cd711e0985983b2e7c162f6504
brendon <108491991+BrendonTheDev@users.noreply.github.com>
2024-02-06 09:48:14 -0500

    Routing with express complete using app.get, app.use and res.render.

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ const express = require("express");
 
 const app = express();
 
+//register view engine
+app.set("view engine", "ejs");
+
 // Listen For Requests
 app.listen(3000);
 
-//register view engine
-app.set("view engine", "ejs");
+//GET "render" home page
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//GET "render" about page
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+//GET "render" contact page
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+//404 use "render" 404 page
+app.use((req, res) => {
+  res.render("404");
+});
